185986af3a1875c0336e9798091a4943ecfa3c91
reynaldo57 <reynaldo57@hotmail.com>
2022-10-28 23:57:54 -0500

    marker

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_codigo3_maps/utils/map_style.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Map<MarkerId, Marker> _markers = {};
 
   final _initialCameraPosition = CameraPosition(target: const LatLng(
       -12.1847695,
@@ -22,8 +29,21 @@ class HomePage extends StatelessWidget {
         scrollGesturesEnabled: true,
         zoomControlsEnabled: true,
         mapType: MapType.normal,
+        markers: _markers.values.toSet(),
         onMapCreated: (GoogleMapController controller){
           controller.setMapStyle(jsonEncode(mapStyle));
+        },
+        onTap: (LatLng position){
+          final markerId = MarkerId(_markers.length.toString());
+          final marker = Marker(
+              markerId: markerId,
+              position: position,
+          );
+          _markers[markerId] = marker;
+
+          setState(() {
+
+          });
         },
       ),
     );
